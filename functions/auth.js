638a0496af74569b1e4b5c473b928bd3d0dc6b3e
Nathanaël Derouineau Leroux <derouineaunathanael@gmail.com>
2026-05-12 12:11:13 +0200

    modif auth

--- functions/auth.js
+++ functions/auth.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+export async function onRequestGet(context) {
   const { request, env } = context;
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
@@ -10,7 +10,10 @@ export async function onRequest(context) {
 
   const response = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
-    headers: { "Content-Type": "application/json", "Accept": "application/json" },
+    headers: { 
+      "Content-Type": "application/json", 
+      "Accept": "application/json" 
+    },
     body: JSON.stringify({
       client_id: env.GITHUB_CLIENT_ID,
       client_secret: env.GITHUB_CLIENT_SECRET,
@@ -21,13 +24,21 @@ export async function onRequest(context) {
   const data = await response.json();
   const token = data.access_token;
 
-  const script = `
-    <script>
-      window.opener.postMessage(
-        'authorization:github:success:{"token":"${token}","provider":"github"}',
-        '*'
-      );
-    </script>
-  `;
-  return new Response(script, { headers: { "Content-Type": "text/html" } });
+  const script = `<!DOCTYPE html><html><body><script>
+    (function() {
+      function receiveMessage(e) {
+        console.log("receiveMessage %o", e);
+        window.opener.postMessage(
+          'authorization:github:success:{"token":"${token}","provider":"github"}',
+          e.origin
+        );
+      }
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    })()
+  </sc` + `ript></body></html>`;
+
+  return new Response(script, { 
+    headers: { "Content-Type": "text/html" } 
+  });
 }
